7ead336b584b7ff249af0713bf4dcbfd8ebd2e70
Deepak Kumar <125193239+Imm0rtal001@users.noreply.github.com>
2026-08-28 10:54:23 +0530

    Update CineFreak.js

--- providers/CineFreak.js
+++ providers/CineFreak.js
@@ -10,7 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const PROVIDER_NAME = 'CineFreak';
 const BASE_URL = 'https://cinefreak.net';
-const ECLIPSIA_BASE = 'https://new5.cinecloud.site';
+const ECLIPSIA_BASE = 'https://cinefreak.net';
 const TMDB_API_KEY = '307b7b8ef035c6aa336900aef4e203bd';
 const HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36',
